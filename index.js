4aaf91178ef114920da37aa5cb79d52af6f71017
anirban1592 <das.nayans@gmail.com>
2022-10-27 23:53:38 +0100

    tes

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+const app = express();
+
+app.get('/check', (req,res) => {
+
+  
+    res.send('hello world !!');
+});
+
+app.listen(8000, () => {
+    console.log('listening on port 8000');
+});
